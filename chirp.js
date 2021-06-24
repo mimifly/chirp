23d12158dd9b2f9d6256b6e7d31550e5518e8f85
mimifly <codemenerd@gmail.com>
2021-06-24 22:07:58 +0100

    Add time function and change appearance

--- chirp.js
+++ chirp.js
@@ -5,8 +5,21 @@ const form = document.querySelector("form");
 const chirps = document.querySelector(".chirps");
 const button = document.querySelector(".submit");
 const isblank = /[^\s]/;
-let lastStamp = 0;
 const dayInUnix = 86400000;
+let lastStamp = 0;
+let chirpsArray = [];
+
+function elapsedUnix(u) {
+  const time = new Date().valueOf();
+  let elapsed = time - u;
+  if (elapsed > 3600000) {
+    return Math.round(elapsed / 3600000) + " hr ago";
+  } else if (elapsed / 60000 < 1) {
+    return "now";
+  } else {
+    return Math.round(elapsed / 60000) + " m ago";
+  }
+}
 
 textarea.addEventListener("input", (event) => {
   textarea.style.height = textarea.style.minHeight;
@@ -34,9 +47,19 @@ chirpsStorage.on("value", (snapshot) => {
       database.ref("chirps/" + key).remove();
     } else if (key > lastStamp) {
       lastStamp = key;
-      const newChirp = document.createElement("li");
-      newChirp.textContent = dataFromSnapshot[key];
-      chirps.insertBefore(newChirp, chirps.firstChild);
+      chirpsArray.push({ time: key, message: dataFromSnapshot[key] });
+      let chirpsHTML = chirpsArray
+        .map((chirp) => {
+          return ` 
+        <li>
+          <p class="time">${elapsedUnix(chirp.time)}</p>
+          <p class="message">${chirp.message}</p>
+        </li>
+        `;
+        })
+        .reverse()
+        .join("");
+      chirps.innerHTML = chirpsHTML;
     }
   }
   textarea.value = "";
